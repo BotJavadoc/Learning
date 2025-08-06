e46344b6e3c12030b7bcf2434cb1f286dcacf57f
Abhishek G <abhishekabu0155@gmail.com>
2025-08-06 22:56:04 +0530

    "learnign about maps"

---
+++ collectionlearn/MapLearning.java
@@ -0,0 +1,27 @@
+package collectionlearn;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapLearning {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "one");
+        map.put("2", "two");
+         map.put("6", "two");
+        map.put("3","three");
+        System.out.println(map);
+        ArrayList<String> a = new ArrayList<>();
+        a.add("one");
+        a.add("two");
+        a.add("three");
+        a.add("two");
+        System.out.println(a);
+        for(Map.Entry<String,String> entry : map.entrySet()) {
+            System.out.println("Key : " +entry.getKey() + " Value : "+ entry.getValue()+" "+Collections.frequency(a, entry.getValue()));
+
+        }
+    }
+}
