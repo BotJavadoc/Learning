f9471c17ef245ce31a4e1573eca098254cd7654c
Abhishek G <abhishekabu0155@gmail.com>
2025-09-25 23:06:04 +0530

    daily commit

--- collectionlearn/DSAPlayaera.java
+++ collectionlearn/DSAPlayaera.java
@@ -1,25 +1,18 @@
 package collectionlearn;
-
+import java.util.*;
 public class DSAPlayaera {
     public static void main(String[] args) {
-        int nums[] = {7,1,5,3,6,4};
-        int min =nums[0];
-        int pos=0;
-        int maxProf =0;
-
-        for(int i=0;i<nums.length;i++) {
-            if(min>nums[i]) {
-                min=nums[i];
-                pos = i+1;
-            }
+        int nums[] = {61 ,61 ,17};
+        int maxi = nums[nums.length-1];
+        ArrayList<Integer> res = new ArrayList<>();
+        res.add(maxi);
+        for(int i=nums.length-2;i>=0;i--) {
+           if(maxi<=nums[i]) {
+            maxi = nums[i];
+            res.add(nums[i]);
+           }
         }
-        for(int i=pos;i<nums.length;i++) {
-            if(maxProf<nums[i]) {
-                maxProf = nums[i];
-            }
-        }
-        System.out.println("Max profit is "+maxProf);
-
-        System.out.println("Min number is "+min+" and its position is "+pos);
-}
+        Collections.reverse(res);
+        System.out.println(res);
+    }
 }
