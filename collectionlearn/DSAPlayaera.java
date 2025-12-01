a0cd3242de02fbb75aac1685454a89a31a366005
Abhishek G <abhishekabu0155@gmail.com>
2025-12-01 22:11:36 +0530

    daily commit 2.0

--- collectionlearn/DSAPlayaera.java
+++ collectionlearn/DSAPlayaera.java
@@ -1,18 +1,24 @@
 package collectionlearn;
 import java.util.*;
+
 public class DSAPlayaera {
     public static void main(String[] args) {
-        int nums[] = {61 ,61 ,17};
-        int maxi = nums[nums.length-1];
-        ArrayList<Integer> res = new ArrayList<>();
-        res.add(maxi);
-        for(int i=nums.length-2;i>=0;i--) {
-           if(maxi<=nums[i]) {
-            maxi = nums[i];
-            res.add(nums[i]);
-           }
+        int nums[] = {9,1,4,7,3,-1,0,5,8,-1,6};
+        Arrays.sort(nums);
+        
+        int count = 0;
+        int max =0;
+        for(int i=1;i<nums.length;i++) {
+            System.out.println("Incoming array nums[i] = "+nums[i]+" nums[i-1] "+nums[i-1] + " and diff is " + (nums[i]-nums[i-1]));
+            if(nums[i]-nums[i-1]==1) {
+                count++;
+            } else {
+                if(max<count) {
+                    max = count;
+                }
+                count = 0; 
+            }
         }
-        Collections.reverse(res);
-        System.out.println(res);
+        System.err.println(count+1);
     }
 }
