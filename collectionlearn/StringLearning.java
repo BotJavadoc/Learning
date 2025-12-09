cf0f7dd3eaa6ebf26c56cc218307ada4646635f0
Abhishek G <abhishekabu0155@gmail.com>
2025-12-09 22:56:48 +0530

    daily  commit

--- collectionlearn/StringLearning.java
+++ collectionlearn/StringLearning.java
@@ -1,28 +1,22 @@
 package collectionlearn;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class StringLearning {
 public static void main(String[] args) {
-    String s = "swiss";
-    Map<Character, Integer>  map = new HashMap();
+    String s = "badc"; 
+    String t = "baba";
+    HashMap<Character,Character> map = new HashMap<>();
     for(int i=0;i<s.length();i++) {
-       
-       if(map.containsKey(s.charAt(i))) {
-                map.put(s.charAt(i),map.get(s.charAt(i))+1);     
-       } else {
-        map.put(s.charAt(i),1);
-       }
+        if(map.containsKey(s.charAt(i))) {
+            System.err.println(s.charAt(i));
+            if(map.get(s.charAt(i))!= t.charAt(i)) {       
+            break;
+        }
+        } else {
+            map.put(s.charAt(i),t.charAt(i));
+        }
     }
-    
-   for(int i=0;i<s.length();i++) {
-    if(map.get(s.charAt(i))==1)  {
-        System.out.println(map.get(s.charAt(i)));
-        System.out.println("found :"+s.indexOf(s.charAt(i)) );
-        break;
-    
-    }
-   }
+    System.err.println(map);
 }
 }
