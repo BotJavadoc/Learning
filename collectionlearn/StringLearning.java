01dce042f793d3d187c258fc4290e9e8065fdfd8
Abhishek G <abhishekabu0155@gmail.com>
2025-12-16 22:51:01 +0530

    daily commit

--- collectionlearn/StringLearning.java
+++ collectionlearn/StringLearning.java
@@ -1,29 +1,30 @@
 package collectionlearn;
 
-import java.util.*;;
+import java.util.*;
 
 public class StringLearning {
 public static void main(String[] args) {
-    String s = "abcde"; 
-    String t = "abced";
-    char sArry [] = s.toCharArray();
-    char tArry [] = t.toCharArray();
-    for(int i=0;i<sArry.length;i++) {
-    
-        if(Arrays.equals(sArry, tArry)) {
-            
-        } else {
-                rotate(sArry);
+    String s = "rrrr    aaaajj"; 
+    Map<Character, Integer> freq = new TreeMap<>();
+    TreeMap<Integer, ArrayList<Character>> list = new TreeMap<>();
+    for(int i=0;i<s.length();i++) {
+        if(freq.containsKey(s.charAt(i))) {
+            freq.put(s.charAt(i), freq.get(s.charAt(i))+1);
+        } else{
+            freq.put(s.charAt(i),1);
         }
     }
-   
+    for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
+    list.computeIfAbsent(entry.getValue(), k -> new ArrayList<>())
+        .add(entry.getKey());
+    }
+   for (Map.Entry<Integer, ArrayList<Character>> entry : list.descendingMap().entrySet()) {
+    System.out.println(entry.getKey() + " -> " + entry.getValue());
 }
-public static char [] rotate(char [] sArry) {
-    char lastElement = sArry[sArry.length - 1];
-     for (int i = sArry.length - 1; i > 0; i--) {
-            sArry[i] = sArry[i - 1];
-        }
-        sArry[0] = lastElement;
-    return sArry;
-}
+
+
+    System.err.println(list);
+    }   
+
+    
 }
