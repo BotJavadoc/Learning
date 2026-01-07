bd41515660a4cdd8319caf72edaa5116fa7c6a8d
Abhishek G <abhishekabu0155@gmail.com>
2026-01-07 22:29:31 +0530

    daily commit

--- collectionlearn/DSAPlayaera.java
+++ collectionlearn/DSAPlayaera.java
@@ -1,24 +1,19 @@
 package collectionlearn;
-import java.util.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DSAPlayaera {
     public static void main(String[] args) {
-        int nums[] = {9,1,4,7,3,-1,0,5,8,-1,6};
-        Arrays.sort(nums);
-        
-        int count = 0;
-        int max =0;
-        for(int i=1;i<nums.length;i++) {
-            System.out.println("Incoming array nums[i] = "+nums[i]+" nums[i-1] "+nums[i-1] + " and diff is " + (nums[i]-nums[i-1]));
-            if(nums[i]-nums[i-1]==1) {
-                count++;
-            } else {
-                if(max<count) {
-                    max = count;
-                }
-                count = 0; 
+        String s = "aabcbaa";
+        char [] arrayChar = s.toCharArray();
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0;i<arrayChar.length;i++) {
+            for(int j = i; i<arrayChar.length;i++) {
+
+                
             }
         }
-        System.err.println(count+1);
     }
+       
 }
