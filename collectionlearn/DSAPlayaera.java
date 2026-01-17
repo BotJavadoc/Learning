7854b19f4ffcdeb534199ad0ad287d951f08b21c
Abhishek G <abhishekabu0155@gmail.com>
2026-01-17 12:36:57 +0530

    daily commit

--- collectionlearn/DSAPlayaera.java
+++ collectionlearn/DSAPlayaera.java
@@ -1,19 +1,38 @@
 package collectionlearn;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class DSAPlayaera {
     public static void main(String[] args) {
-        String s = "aabcbaa";
-        char [] arrayChar = s.toCharArray();
-        Map<String, Integer> map = new HashMap<>();
-        for(int i=0;i<arrayChar.length;i++) {
-            for(int j = i; i<arrayChar.length;i++) {
+       int n = 3;
 
-                
-            }
+        // List to store results
+        List<String> result = new ArrayList<>();
+
+        // Start recursion with empty string
+        DSAPlayaera.generate(n, "", result);
+
+        // Print results
+        for (String s : result) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+    }
+
+    static void generate(int n, String curr, List<String> result) {
+        // Base case: if length is n, add to result
+        if (curr.length() == n) {
+            result.add(curr);
+            return;
+        }
+
+        // Always try adding '0'
+        generate(n, curr + "0", result);
+
+        // Add '1' only if previous char is not '1'
+        if (curr.isEmpty() || curr.charAt(curr.length() - 1) != '1') {
+            generate(n, curr + "1", result);
         }
     }
+
        
 }
