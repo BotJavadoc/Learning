63394727f53dba41247b927330264ba9be8c2e3e
Abhishek G <abhishekabu0155@gmail.com>
2026-01-20 09:32:21 +0530

    daily commit

--- collectionlearn/DSAPlayaera.java
+++ collectionlearn/DSAPlayaera.java
@@ -7,48 +7,33 @@ import java.util.List;
 public class DSAPlayaera {
 
     public static void main(String[] args) {
-        int[] nums = {3,5,6,7};
+        int[] nums = {2,3,6,7};
         Arrays.sort(nums);
-        int k = 9;
+        int k = 7;
+        
         List<List<Integer>> list = new ArrayList<>();
         printSub(nums, 0, new ArrayList<>(), list, k);
 
-        System.out.println("All subsequences:");
+        System.out.println("All subsequences:"+list);
         System.out.println(list.size());
     }
 
     public static void printSub(int[] nums,int index,List<Integer> current,List<List<Integer>> result, int k) {
         if(index == nums.length) {
-            if(helper(k,current)){
             System.err.println(" index on each rec "+index);
+            if(k == 0) {
             result.add(new ArrayList<>(current));
-            System.out.println(current);
             }
-          //  System.out.println(result);
+            System.out.println(current);
             return;
         }
+        if(nums[index]<=k){
         current.add(nums[index]);
-        printSub(nums, index+1, current, result,k);
+        printSub(nums, index, current, result,k-nums[index] );
         current.remove(current.size()-1);
+        }
         System.err.println(" index on each rec "+index);
         printSub(nums, index+1, current, result,k);
     }
-
-   public static Boolean helper(int k, List<Integer> list) {
-    if (list.isEmpty()) return false;
-    int sum = 0;
-
-    if(list.size()==1) {
-        sum = list.get(0) +list.get(0);
-        System.err.println("single val "+sum);
-        if(sum<=k) {
-            return true;
-        }
-    }
-    
-    sum = list.get(0) + list.get(list.size()-1);
- System.err.println("outside unique val "+sum);
-    return sum <= k;
-}
 
 }
